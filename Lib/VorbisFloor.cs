e6a3556aa892bdf56bf0c3e394c3f78ee6ffe247
Renaud Bédard <renaud.bedard@gmail.com>
2012-09-06 12:29:48 -0400

    Reverted to old version of Floor1 because of problems with some ogg files

--- Lib/VorbisFloor.cs
+++ Lib/VorbisFloor.cs
@@ -229,6 +229,7 @@ namespace NVorbis
             VorbisCodebook[] _classMasterbooks;
             VorbisCodebook[][] _subclassBooks;
             int[][] _subclassBookIndex;
+            List<int> y = new List<int>(29); 
 
             static int[] _rangeLookup = { 256, 128, 86, 64 };
             static int[] _yBitsLookup = { 8, 7, 7, 6 };
@@ -342,30 +343,29 @@ namespace NVorbis
                 }
 
                 public int[] Posts;
-                public int PostCount;
             }
 
             internal override PacketData UnpackPacket(DataPacket packet, int blockSize)
             {
-                var data = ReadPosts(packet);
-                data.BlockSize = blockSize;
+                var data = new PacketData1 { BlockSize = blockSize };
+                data.Posts = ReadPosts(packet);
                 return data;
             }
 
             internal override void Apply(PacketData packetData, float[] residue)
             {
-                if (packetData != null)
-                {
-                    var data = packetData as PacketData1;
-                    if (data == null) throw new InvalidDataException("Incorrect packet data!");
+                var data = packetData as PacketData1;
+                if (data == null) throw new InvalidDataException("Incorrect packet data!");
 
-                    var stepFlags = UnwrapPosts(data);
+                if (data.Posts != null)
+                {
+                    var stepFlags = UnwrapPosts(data.Posts);
 
                     var n = data.BlockSize / 2;
 
                     var lx = 0;
                     var ly = data.Posts[0] * _multiplier;
-                    for (int i = 1; i < data.PostCount; i++)
+                    for (int i = 1; i < data.Posts.Length; i++)
                     {
                         var idx = _sortIdx[i];
 
@@ -386,21 +386,27 @@ namespace NVorbis
                     {
                         RenderLineMulti(lx, ly, n, ly, residue);
                     }
-
-                    ACache.Return(ref data.Posts);
                 }
             }
 
-            PacketData1 ReadPosts(DataPacket packet)
+            //internal override float[] DecodePacket(DataPacket packet, int blockSize)
+            //{
+            //    var posts = ReadPosts(packet);
+            //    if (posts == null) return null;
+
+            //    var stepFlags = UnwrapPosts(posts);
+            //    return ComputeCurve(posts, stepFlags, blockSize);
+            //}
+
+            int[] ReadPosts(DataPacket packet)
             {
                 if (!packet.ReadBit()) return null;
 
                 try
                 {
-                    var postCount = 2;
-                    var posts = ACache.Get<int>(64);
-                    posts[0] = (int)packet.ReadBits(_yBits);
-                    posts[1] = (int)packet.ReadBits(_yBits);
+                    y.Clear();
+                    y.Add((int)packet.ReadBits(_yBits));
+                    y.Add((int)packet.ReadBits(_yBits));
 
                     for (int i = 0; i < _partitionClass.Length; i++)
                     {
@@ -419,17 +425,16 @@ namespace NVorbis
                             cval >>= cbits;
                             if (book != null)
                             {
-                                posts[postCount] = (int)book.DecodeScalar(packet);
+                                y.Add((int)book.DecodeScalar(packet));
                             }
                             else
                             {
-                                posts[postCount] = 0;
+                                y.Add(0);
                             }
-                            ++postCount;
                         }
                     }
 
-                    return new PacketData1 { Posts = posts, PostCount = postCount };
+                    return y.ToArray();
                 }
                 catch (EndOfStreamException)
                 {
@@ -437,24 +442,24 @@ namespace NVorbis
                 }
             }
 
-            bool[] UnwrapPosts(PacketData1 data)
+            bool[] UnwrapPosts(int[] posts)
             {
-                var stepFlags = ACache.Get<bool>(data.PostCount, false);
+                var stepFlags = ACache.Get<bool>(posts.Length, false);// new bool[posts.Length];
                 stepFlags[0] = true;
                 stepFlags[1] = true;
 
-                var finalY = ACache.Get<int>(data.PostCount);
-                finalY[0] = data.Posts[0];
-                finalY[1] = data.Posts[1];
+                var finalY = ACache.Get<int>(posts.Length);// new int[posts.Length];
+                finalY[0] = posts[0];
+                finalY[1] = posts[1];
 
-                for (int i = 2; i < data.PostCount; i++)
+                for (int i = 2; i < posts.Length; i++)
                 {
                     var lowOfs = _lNeigh[i];
                     var highOfs = _hNeigh[i];
 
                     var predicted = RenderPoint(_xList[lowOfs], finalY[lowOfs], _xList[highOfs], finalY[highOfs], _xList[i]);
-
-                    var val = data.Posts[i];
+                    
+                    var val = posts[i];
                     var highroom = _range - predicted;
                     var lowroom = predicted;
                     int room;
@@ -504,15 +509,48 @@ namespace NVorbis
                     }
                 }
 
-                for (int i = 0; i < data.PostCount; i++)
+                for (int i = 0; i < posts.Length; i++)
                 {
-                    data.Posts[i] = finalY[i];
+                    posts[i] = finalY[i];
                 }
 
                 ACache.Return(ref finalY);
 
                 return stepFlags;
             }
+
+            //float[] ComputeCurve(int[] y, bool[] stepFlags, int blockSize)
+            //{
+            //    var n = blockSize / 2;
+
+            //    var floor = ACache.Get<float>(blockSize, false);
+
+            //    var lx = 0;
+            //    var ly = y[0] * _multiplier;
+            //    for (int i = 1; i < y.Length; i++)
+            //    {
+            //        var idx = _sortIdx[i];
+
+            //        if (stepFlags[idx])
+            //        {
+            //            var hx = _xList[idx];
+            //            var hy = y[idx] * _multiplier;
+            //            if (lx < n) RenderLine(lx, ly, Math.Min(hx, n), hy, floor);
+            //            lx = hx;
+            //            ly = hy;
+            //        }
+            //        if (lx >= n) break;
+            //    }
+
+            //    ACache.Return(ref stepFlags);
+
+            //    if (lx < n)
+            //    {
+            //        RenderLine(lx, ly, n, ly, floor);
+            //    }
+
+            //    return floor;
+            //}
 
             int RenderPoint(int x0, int y0, int x1, int y1, int X)
             {
@@ -530,6 +568,33 @@ namespace NVorbis
                     return y0 + off;
                 }
             }
+
+            //void RenderLine(int x0, int y0, int x1, int y1, float[] v)
+            //{
+            //    var dy = y1 - y0;
+            //    var adx = x1 - x0;
+            //    var ady = Math.Abs(dy);
+            //    var sy = 1 - (((dy >> 31) & 1) * 2);
+            //    var b = dy / adx;
+            //    var x = x0;
+            //    var y = y0;
+            //    var err = -adx;
+                
+            //    v[x0] = inverse_dB_table[y0];
+            //    ady -= Math.Abs(b) * adx;
+                
+            //    while (++x < x1)
+            //    {
+            //        y += b;
+            //        err += ady;
+            //        if (err >= 0)
+            //        {
+            //            err -= adx;
+            //            y += sy;
+            //        }
+            //        v[x] = inverse_dB_table[y];
+            //    }
+            //}
 
             void RenderLineMulti(int x0, int y0, int x1, int y1, float[] v)
             {
