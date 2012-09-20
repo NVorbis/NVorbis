a3b08ecfa5ce8161d236be1c2f982b23c7180c02
Andrew Ward <afward@gmail.com>
2012-09-20 10:45:01 -0500

    * Added multi-stream support to VorbisWaveReader * Added several info properties to VorbisWaveReader

--- NAudioSupport/VorbisWaveReader.cs
+++ NAudioSupport/VorbisWaveReader.cs
@@ -12,6 +12,8 @@ using System.Text;
 
 namespace NVorbis.NAudioSupport
 {
+    // TODO: figure out how to handle concatenated streams!
+
     public class VorbisWaveReader : NAudio.Wave.WaveStream, IDisposable, NAudio.Wave.ISampleProvider, NAudio.Wave.IWaveProvider
     {
         NVorbis.VorbisReader _reader;
@@ -75,11 +77,82 @@ namespace NVorbis.NAudioSupport
 
         public int Read(float[] buffer, int offset, int count)
         {
-            count = _reader.ReadSamples(buffer, offset, count);
+            var readCount = _reader.ReadSamples(buffer, offset, count);
 
-            _position += count * 4;
+            //if (readCount < count)
+            //{
+            //    // we're almost certainly at the end of the stream...  maybe try to read the next stream?
+            //    if (ConcatenateStreams)
+            //    {
+            //        if (_reader.SwitchStreams(_reader.StreamIndex))
+            //        {
+            //            readCount += Read(buffer, offset + readCount, count - readCount);
 
-            return count;
+            //            _position = (long)(_reader.DecodedTime.TotalSeconds * _waveFormat.SampleRate * _waveFormat.Channels * 4);
+            //        }
+            //    }
+            //}
+            //else
+            {
+                _position += readCount * 4;
+            }
+
+            return readCount;
         }
+
+        public int StreamCount
+        {
+            get { return _reader.StreamCount; }
+        }
+
+        //public bool ConcatenateStreams { get; set; }
+
+        public int CurrentStream
+        {
+            get { return _reader.StreamIndex; }
+            set
+            {
+                if (!_reader.SwitchStreams(value))
+                {
+                    throw new System.IO.InvalidDataException("The selected stream is not a valid Vorbis stream!");
+                }
+                _position = (long)(_reader.DecodedTime.TotalSeconds * _waveFormat.SampleRate * _waveFormat.Channels * 4);
+            }
+        }
+
+        /// <summary>
+        /// Gets the encoder's upper bitrate of the current selected Vorbis stream
+        /// </summary>
+        public int UpperBitrate { get { return _reader.UpperBitrate; } }
+
+        /// <summary>
+        /// Gets the encoder's nominal bitrate of the current selected Vorbis stream
+        /// </summary>
+        public int NominalBitrate { get { return _reader.NominalBitrate; } }
+
+        /// <summary>
+        /// Gets the encoder's lower bitrate of the current selected Vorbis stream
+        /// </summary>
+        public int LowerBitrate { get { return _reader.LowerBitrate; } }
+
+        /// <summary>
+        /// Gets the encoder's vendor string for the current selected Vorbis stream
+        /// </summary>
+        public string Vendor { get { return _reader.Vendor; } }
+
+        /// <summary>
+        /// Gets the comments in the current selected Vorbis stream
+        /// </summary>
+        public string[] Comments { get { return _reader.Comments; } }
+
+        /// <summary>
+        /// Gets the number of bits read that are related to framing and transport alone
+        /// </summary>
+        public long ContainerOverheadBits { get { return _reader.ContainerOverheadBits; } }
+
+        /// <summary>
+        /// Gets stats from each decoder stream available
+        /// </summary>
+        public IVorbisStreamStatus[] Stats { get { return _reader.Stats; } }
     }
 }
