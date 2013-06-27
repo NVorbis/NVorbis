4ed1887b61e24f09d3007428e089d55b47c13c55
Andrew Ward <afward@gmail.com>
2013-06-27 14:18:58 -0500

    * Bump version numbers * Update nuspec files * Update README

--- NVorbis/Properties/AssemblyInfo.cs
+++ NVorbis/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.5.6.0")]
-[assembly: AssemblyFileVersion("0.5.6.0")]
+[assembly: AssemblyVersion("0.6.0.0")]
+[assembly: AssemblyFileVersion("0.6.0.0")]
