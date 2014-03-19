0c8b03f835829e94852abe0089cf4b6437338da5
Andrew Ward <afward@gmail.com>
2014-03-19 08:13:04 -0500

    * Update version number * Publish nuget package

--- NVorbis/Properties/AssemblyInfo.cs
+++ NVorbis/Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.8.1.0")]
-[assembly: AssemblyFileVersion("0.8.1.0")]
+[assembly: AssemblyVersion("0.8.2.0")]
+[assembly: AssemblyFileVersion("0.8.2.0")]
